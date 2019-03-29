0c92ab784da1ce75a609b4853f2083dc7afabde3
CHENGHSIEN Lo <aszx99370765@gmail.com>
2019-03-29 15:58:44 +0800

    add no chance mode

--- password.py
+++ password.py
@@ -1,10 +1,12 @@
-num = 3
+num = 3 #總共的機會
 while num > 0:
 	password = input('press your password: ')
-	if password == 'a123456':
+	if password == 'a123456': #判斷密碼是否正確
 		print('Login Succeessful')
 		break
 	else:
 		num = num - 1
 		if num > 0:
 			print('password incorrect. you have', num, 'chance')
+		else:
+			print("you don't have any chance")
